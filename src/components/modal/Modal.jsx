d43d0ea5050a3c8e7f5acfa0378d171a96ee02fd
John Hysong <99369137+johnatticus@users.noreply.github.com>
2023-05-11 17:59:28 -0400

    worked on project modal

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -4,13 +4,16 @@ import "./modal.css";
 const ProjectModal = ({ setIsOpen, ...item }) => {
     return (
       <>
-        <div id="myModal" className="project-modal" onClick={() => setIsOpen(false)}>
+        <div className="project-modal" onClick={() => setIsOpen(false)}>
             <div className="project-modal-content">
             <div className="project-modal-header">
 
                 <p>{item.title}</p>
                 <div className="modal-max-min">
-                <span className="close" onClick={() => setIsOpen(false)}>&times;</span>
+                  <div className="modal-header-square" onClick={() => setIsOpen(false)}><p>_</p></div>
+                  <div className="modal-header-square" onClick={() => setIsOpen(false)}><p>&#9744;</p></div>
+                  <div className="modal-header-square" onClick={() => setIsOpen(false)}>x</div>
+                {/* <span className="close" onClick={() => setIsOpen(false)}>&times;</span> */}
                 </div>
             </div>
             <div className="project-modal-body">
@@ -25,3 +28,10 @@ const ProjectModal = ({ setIsOpen, ...item }) => {
   };
   
   export default ProjectModal;
+
+
+
+
+// import { RxDash } from "react-icons/rx";
+// import { RxSquare } from "react-icons/rx";
+// import { RxCross2 } from "react-icons/rx";
